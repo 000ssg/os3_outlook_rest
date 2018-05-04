1a13d6de69dc1b06e0952dca261214d63c0a89ef
000ssg@gmail.com <000ssg@gmail.com>
2018-05-04 13:39:37 +0300

    play with permissions

--- src/main/java/org/openshift/quickstarts/undertow/servlet/OutlookAuth.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/OutlookAuth.java
@@ -23,7 +23,7 @@ public class OutlookAuth {
     String clientSecret;
     String responseType = "code";
     String grantType = "authorization_code";
-    String scope = "openid+Mail.Read";
+    String scope = "openid+Mail.Read+User.Read.All";
     //
     String code;
 
@@ -162,7 +162,7 @@ public class OutlookAuth {
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Accept", "application/json");
         conn.setRequestProperty("Authorization", "Bearer " + token);
-        //conn.setRequestProperty("X-AnchorMailbox","jason@contoso.onmicrosoft.com");
+        //conn.setRequestProperty("X-AnchorMailbox","sergey.sidorov@digia.com");
 
         conn.connect();
 
@@ -219,11 +219,10 @@ public class OutlookAuth {
     }
 
     public static void main(String[] args) throws Exception {
-        OutlookAuth oa = new OutlookAuth("sergey.sidorov@digia.com");
-        if (oa.doAuth()) {
-            System.out.println("Outlook auth call succeeded");
-        } else {
-            System.out.println("Outlook auth call failed");
-        }
+        OutlookAuth oa = new OutlookAuth("86b0f61c-2e69-41df-bdbe-49ebce3f7795");
+        //oa.clientId="86b0f61c-2e69-41df-bdbe-49ebce3f7795";
+        //oa.clientSecret="wGPTTH123+}@ojfukoJK03=";
+        String token="eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEWDhHQ2k2SnM2U0s4MlRzRDJQYjdyY2ZpSEVtbUFkb2xfWXd4Ni03Y1VuZi1NQ255d0lqMDl2cU9SMWhxc1hHS1EzQ2wwcHpDVGdVeTE1VzhFeG5aNkd2T19kaHFLeklTYmlacFFkTTl1bnlBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiaUJqTDFSY3F6aGl5NGZweEl4ZFpxb2hNMllrIiwia2lkIjoiaUJqTDFSY3F6aGl5NGZweEl4ZFpxb2hNMllrIn0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC82ZmNlNGJiOC0zNTAxLTQxYzktYWZjYy1kYjBmYjUxYzdlM2QvIiwiaWF0IjoxNTI1NDI5NTE4LCJuYmYiOjE1MjU0Mjk1MTgsImV4cCI6MTUyNTQzMzQxOCwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhIQUFBQTR2NStrR2xmUE9HbG43eGFrczZ6c2FyYnhOa1kzZXFYMGhIdlRnUWZJd1E9IiwiYW1yIjpbInB3ZCJdLCJhcHBfZGlzcGxheW5hbWUiOiJ0ZXN0IG91dGxvb2sgcmVzdCIsImFwcGlkIjoiODZiMGY2MWMtMmU2OS00MWRmLWJkYmUtNDllYmNlM2Y3Nzk1IiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJTaWRvcm92IiwiZ2l2ZW5fbmFtZSI6IlNlcmdleSIsImlwYWRkciI6IjkxLjIxNy4yNDguMTEiLCJuYW1lIjoiU2lkb3JvdiBTZXJnZXkiLCJvaWQiOiJiMTEzMTM5NS1mOTUwLTQxYmItYmQ1Zi05NjlhYjJhZDM2ZjciLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtMjQzMDY3MTQ2Mi0yODUyOTcxNTUxLTI3OTYwMTEwNTUtMjE0NTIiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzAwMDA4OTMxQjUzMSIsInNjcCI6Ik1haWwuUmVhZCIsInNpZ25pbl9zdGF0ZSI6WyJpbmtub3dubnR3ayIsImttc2kiXSwic3ViIjoiYmdObElSOWVoYnpqMWlPU05FZkVXRjJJZnFoeWgzQ242UXhjZ3otalp6OCIsInRpZCI6IjZmY2U0YmI4LTM1MDEtNDFjOS1hZmNjLWRiMGZiNTFjN2UzZCIsInVuaXF1ZV9uYW1lIjoic2VyZ2V5LnNpZG9yb3ZAZGlnaWEuY29tIiwidXBuIjoic2VyZ2V5LnNpZG9yb3ZAZGlnaWEuY29tIiwidXRpIjoibG1KLVFkZHFlRWUya09RRWduNEVBQSIsInZlciI6IjEuMCJ9.DQLUzOT82P_RZ5n9OV81pLC4Xl9njiy6quxa45CN3hjD6hl1TblrWZvXPF7GpK0vMgeWwaNXANhUWZoih746DVAFvRJHdicUBDSdoBKx61STLZOn-XVL99_9kBAreDKGS1SmldnjAIz-Fl5LbRGl3py5v-Hz_t-nj6BdmqUXtHgXAQOLhoydRDbBaw-nuwIuB5RiJ4OkRwi_NmBBK5zeHIFCdHWxOgoszHds6catMq2v9IUstQhX31w2_t5tb3kLKzQtFPz2iltyJgKV3vqeeAUckY6fTZpmLnuzhNsHoJv-v-mtgdUyje-xW0xSU4VL88eltftbrPW8Zg58GZD8wQ\",\"id_token\":\"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImlCakwxUmNxemhpeTRmcHhJeGRacW9oTTJZayJ9.eyJhdWQiOiI4NmIwZjYxYy0yZTY5LTQxZGYtYmRiZS00OWViY2UzZjc3OTUiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vNmZjZTRiYjgtMzUwMS00MWM5LWFmY2MtZGIwZmI1MWM3ZTNkL3YyLjAiLCJpYXQiOjE1MjU0Mjk1MTgsIm5iZiI6MTUyNTQyOTUxOCwiZXhwIjoxNTI1NDMzNDE4LCJhaW8iOiJBVFFBeS84SEFBQUF5K2kvd3V1VnU3TStxcEphb29OUXF0a1NsYzFqTmNLZ21vL0t4RTB0STVqd05qVk1LdzI0ejdDMDk4aUxPS3I2Iiwic3ViIjoiTENJWDFvSGpyNllfS1hHRFppQXY1NlVFenpLakNNX043S1pPc2hfUTg2YyIsInRpZCI6IjZmY2U0YmI4LTM1MDEtNDFjOS1hZmNjLWRiMGZiNTFjN2UzZCIsInV0aSI6ImxtSi1RZGRxZUVlMmtPUUVnbjRFQUEiLCJ2ZXIiOiIyLjAifQ.QZCGoZXyd0uyx5D8edH0XJ0Jn4vgHkWMcNC7s_B28xzbaArImC1mYNDWxGrjgh3gc4o6rp_TnCYelLCRWhx9enxonCLgX-C2tgbhndiyUAkuvwVUZuBaNdacqE8OnDAYum1TNt4MGiN8DSnWjTutIgK0113Q8s-hchk_Pg9jBSs_7wzgJz3moapyOaJTBCDJyEZ9OKK_PRUESy8dXKHQsuA8YU5X9VH1yhu2PF0CISnPlw8bFrOsK2G8KlnCY5b4CI-D0nlHxjyuFaQOhMo1IUC_SKVMiwfy93mH5TcutEfEdDSP_Dic0z0_ozoguMbf_oDJOH0XRIB7dcGCg_gAFA";
+        String rl=oa.roomsLists(token);
     }
 }
